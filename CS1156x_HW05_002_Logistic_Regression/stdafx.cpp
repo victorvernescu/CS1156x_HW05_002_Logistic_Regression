309f0e88530a56b79ff3a9d0fe50a3003071e391
Victoras <victor.vernescu@gmail.com>
2013-11-06 14:23:01 +0200

    Initial commit

---
+++ CS1156x_HW05_002_Logistic_Regression/stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : source file that includes just the standard includes
+// CS1156x_HW05_002_Logistic_Regression.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
